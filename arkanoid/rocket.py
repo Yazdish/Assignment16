a70905bcd156912f10362e7f890ef20e32c3218e
Yazdan Shouryabi <yazdishouryabi@gmail.com>
2023-10-31 20:35:45 +0330

    Arkanoid almost Done

--- arkanoid/rocket.py
+++ arkanoid/rocket.py
@@ -7,10 +7,23 @@ class Rocket(arcade.Sprite):
         self.center_y = 60
         self.change_x = 0
         self.change_y = 0
+        self.width = 60
+        self.height = 20
+        self.color = arcade.color.BLACK
         self.speed = 4
+        self.score = 0
+        self.life = 3
 
 
-    def move():
-        ...
+    def move(self, game):
+        
+        self.center_x += self.change_x * self.speed
 
-    
+        if self.center_x < 60:
+            self.center_x = 60
+
+        if self.center_x > game.width - 60 :
+            self.center_x = game.width - 60
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
